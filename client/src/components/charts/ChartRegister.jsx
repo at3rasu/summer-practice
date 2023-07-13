07ac25f64bf39b40f0b74785ac5d8cf0972d330a
Artur Imankulov <arturimankulov906@gmail.com>
2023-07-14 01:04:14 +0500

    upgrade statistic

--- client/src/components/charts/ChartRegister.jsx
+++ client/src/components/charts/ChartRegister.jsx
@@ -6,9 +6,9 @@ import {
   BarElement,
   Title,
   Tooltip,
-  Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+import styles from './ChartRegister.module.css'
 
 ChartJS.register(
     CategoryScale,
@@ -16,7 +16,6 @@ ChartJS.register(
     BarElement,
     Title,
     Tooltip,
-    Legend
   );
 
 export const ChartRegister = () => {
@@ -25,23 +24,43 @@ export const ChartRegister = () => {
     datasets: [
       {
         data: [300, 300, 400, 545, 100, 535, 213, 321, 545, 645, 133, 313],
-        borderColor: "#3333ff",
-        backgroundColor: "rgba(0, 0, 255, 0.5)",
+        borderColor: "#F27E80",
+        backgroundColor: "#F27E80",
         fill: true
       },
     ]
   };
 
   const barChart = (
+    <div className={styles.box}>
     <Bar
       type="bar"
-      width={130}
-      height={50}
+      width={230}
+      height={70}
       options={{
-        responsive: true
+        responsive: true,
+        scales:{
+            x:{
+                grid:{
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            y:{
+                grid:{
+                    display:false,
+                    drawBorder: false
+                },
+                ticks:{
+                    display:false
+                }
+            }
+        },
+        borderRadius:8
       }}
       data={barChartData}
     />
+    </div>
   );
   return barChart;
 };
--- client/src/pages/statistics/Statistics.jsx
+++ client/src/pages/statistics/Statistics.jsx
@@ -15,15 +15,40 @@ export const Statistics = () => {
         <div className={styles.page}>
             <HeaderAdmin />
             <div className={styles.container}>
+                <span className={styles.title}>
+                    <h1>Динамика регистраций</h1>    
+                </span>
                 <div className={styles.chart}>
-                    <div className={styles.table}>
-                        <ChartRegister /> 
-                        <ChartCity />
+                    <div className={styles.grid}>
+                        <div className={styles.flex}>
+                            <p>В среднем в месяц</p>
+                            <h1>416</h1>
+                        </div>
+                        <div className={styles.flex}>
+                            <p>Со Свердловской области</p>
+                            <h1>3456</h1>
+                        </div>
+                        <div className={styles.flex}>
+                            <p>Иногородние</p>
+                            <h1>1096</h1>
+                        </div>
+                        <div className={styles.flex}>
+                            <p>Активных пользователей</p>
+                            <h1>4160</h1>
+                        </div>
                     </div>
-                    <div className={styles.table}> 
+                    <div className={styles.table}>
+                        <div className={styles.gridReg}>
+                            <p>Всего зарегистрировано</p>
+                            <h1>4550</h1>
+                        </div>
+                        <ChartRegister /> 
+                        {/* <ChartCity /> */}
+                    </div>
+                    {/* <div className={styles.table}> 
                         <Table />
                         <TableStudent />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
