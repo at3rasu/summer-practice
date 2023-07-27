34ea4fa7bdaf4d27b157a7086423e2264a7e80e6
Artur Imankulov <arturimankulov906@gmail.com>
2023-07-27 18:58:26 +0500

    create data

---
+++ client/src/components/navbar/NavBar.jsx
@@ -0,0 +1,9 @@
+import styles from './NavBar.module.css'
+
+export const NavBar = () =>{
+    return(
+        <div className={styles.component}>
+            
+        </div>
+    )
+}
--- client/src/pages/statistics/GrantStatistics.jsx
+++ client/src/pages/statistics/GrantStatistics.jsx
@@ -1,14 +1,30 @@
-import { SideBar } from '../../components/sidebar/SideBar'
+import { MobileHeader } from '../../components/header/MobileHeader'
 import styles from './Statistics.module.css'
 
 export const GrantStatistics = () =>{
     return(
         <div className={styles.app}>
-            <SideBar/>
-            <div className={styles.conteiner}>
-                <span className={styles.title}>
-                    <h1>Бонусная программа</h1>    
-                </span>
+            <MobileHeader />
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <h1>Участники программы</h1>
+                </div>
+                <div className={styles.data}>
+                    <span>
+                        <p>Студенты:</p>
+                        <h1>2145</h1>
+                    </span>
+                    <hr></hr>
+                    <span>
+                        <p>ВУЗы:</p>
+                        <h1>26</h1>
+                    </span>
+                    <hr></hr>
+                    <span>
+                        <p>Компании:</p>
+                        <h1>32</h1>
+                    </span>
+                </div>
             </div>
         </div>
     )
